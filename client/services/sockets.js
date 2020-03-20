6a2a5fe462a344daf68d5fec396e318584f32c5e
Chris Durden <cdurden@gmail.com>
2020-03-20 00:38:07 -0500

    angularjs-config

--- client/services/sockets.js
+++ client/services/sockets.js
@@ -1,6 +1,6 @@
 angular.module('whiteboard.services.sockets', [])
 .factory('Sockets', function (socketFactory) {
-  var myIoSocket = io.connect();
+  var myIoSocket = io.connect({'path': BASE_HREF+'socket.io');
 
   mySocket = socketFactory({
     ioSocket: myIoSocket
