2a1fdd1e90150fff2a39e515b81e7b25bf24e9a4
Chris Durden <cdurden@gmail.com>
2020-03-21 02:40:04 -0500

    lti

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ passport.use('lti-strategy', new CustomStrategy(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.authenticate('lti-strategy', {failureFlash: true}));
-app.use('/', entry)
+app.use('/lti_launch', entry)
 
 app.post('/', function (req, res) {
   res.send('POST request to the homepage')
