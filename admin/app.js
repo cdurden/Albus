5005004be9e8ebb7718c62085419bb0f8b58a960
Chris Durden <cdurden@gmail.com>
2020-03-21 17:29:42 -0500

    admin

--- admin/app.js
+++ admin/app.js
@@ -2,7 +2,7 @@
 angular.module('whiteboard-admin', ['whiteboard.services.sockets', 'ngRoute']).config(['$routeProvider', '$locationProvider', '$httpProvider',
   function($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
-      .when('/admin', {
+      .when('/admin/', {
           //template: '<div wb-admin-submissions></div>',
           template: 'Testing!<div wb-admin-submissions></div>',
         //  templateUrl: '/admin/views/admin.html',
