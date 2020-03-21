b96e1e414e433a8f9e8fff85d8e79d09a7c88126
Chris Durden <cdurden@gmail.com>
2020-03-21 03:58:24 -0500

    lti

--- server/server.js
+++ server/server.js
@@ -50,6 +50,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 passport.serializeUser(function(user, done) {
+  console.log(user);
   done(null, user.user_id);
 });
 
