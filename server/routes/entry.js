56dd5eecb6206986cd5d4b8a482603b6c07fd997
Chris Durden <cdurden@gmail.com>
2020-03-21 02:39:21 -0500

    lti

--- server/routes/entry.js
+++ server/routes/entry.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function (req, res) {
+router.post('/lti_launch', function (req, res) {
 	req.session.lti_token = req.body;
 	if(req.body.custom_class_name && req.body.lis_course_section_sourcedid){
 		req.session.lti_token.lis_course_section_sourcedid_original = req.body.lis_course_section_sourcedid
@@ -10,7 +10,8 @@ router.post('/', function (req, res) {
 	if(req.body.custom_section_number){
 		req.session.lti_token.lis_course_section_sourcedid = req.session.lti_token.lis_course_section_sourcedid + '-' + req.body.custom_section_number
 	}
-	const url = 'http://' + process.env.PRESENT_PATH + ':' + process.env.PROXY_PORT
+	//const url = 'http://' + process.env.PRESENT_PATH + ':' + process.env.PROXY_PORT
+	const url = '/';
 
 	res.redirect(url);
 })
