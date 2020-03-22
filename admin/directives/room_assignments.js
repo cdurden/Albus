6e1bd0173407efbfde50b26486992683ab29c995
Chris Durden <cdurden@gmail.com>
2020-03-21 19:05:24 -0500

    admin

--- admin/directives/room_assignments.js
+++ admin/directives/room_assignments.js
@@ -13,29 +13,29 @@ angular.module('whiteboard-admin')
     templateUrl: 'templates/room_assignments.html',
     link: function(scope, element, attrs) {
       console.log("calling link function");
-function injectLibFromStack(callback){
-    if(libs.length > 0){
-
-      //grab the next item on the stack
-      var nextLib = libs.shift();
-      var headTag = document.getElementsByTagName('head')[0];
-
-      //create a script tag with this library
-      var scriptTag = document.createElement('script');
-      scriptTag.src = nextLib;
-
-      //when successful, inject the next script
-      scriptTag.onload = function(e){
-        console.log("---> loaded: " + e.target.src);
-        injectLibFromStack(callback);
-      };
-
-      //append the script tag to the <head></head>
-      headTag.appendChild(scriptTag);
-      console.log("injecting: " + nextLib);
-    }
-    else return callback();
-}
+      function injectLibsFromStack(callback){
+          if(libs.length > 0){
+      
+            //grab the next item on the stack
+            var nextLib = libs.shift();
+            var headTag = document.getElementsByTagName('head')[0];
+      
+            //create a script tag with this library
+            var scriptTag = document.createElement('script');
+            scriptTag.src = nextLib;
+      
+            //when successful, inject the next script
+            scriptTag.onload = function(e){
+              console.log("---> loaded: " + e.target.src);
+              injectLibFromStack(callback);
+            };
+      
+            //append the script tag to the <head></head>
+            headTag.appendChild(scriptTag);
+            console.log("injecting: " + nextLib);
+          }
+          else return callback();
+      }
       var libs = ["https://raw.githack.com/SortableJS/Sortable/master/Sortable.js", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"]
       injectLibsFromStack(function() {
         Sortable.create(productList, {
