23c22bd43b697d35ad8abfc5d55d5abd2024bc0c
Chris Durden <cdurden@gmail.com>
2020-03-23 17:06:31 -0500

    admin

--- client/js/chat.js
+++ client/js/chat.js
@@ -4,3 +4,14 @@ function openChatForm() {
 function closeChatForm() {
       document.getElementById("chat-popup").style.display = "none";
 }
+function toggleChatForm() {
+  var x = document.getElementById("chat-popup");
+  var btn = document.getElementById("chat-toggle-button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    btn.innerHTML = "Hide chat";
+  } else {
+    x.style.display = "none";
+    btn.innerHTML = "Show chat";
+  }
+}
