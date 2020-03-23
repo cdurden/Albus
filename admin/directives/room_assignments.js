fe517905af809cc2c4b58905a6fff606df97f76c
Chris Durden <cdurden@gmail.com>
2020-03-22 20:34:28 -0500

    admin

--- admin/directives/room_assignments.js
+++ admin/directives/room_assignments.js
@@ -26,7 +26,25 @@ angular.module('whiteboard-admin')
         $(".roomList").each(function(i, elmt) {
           Sortable.create(elmt, {
             group: 'rooms'
-          });
+          }).onChange(
+            $('.roomList').each(function(i,room_elmt) { 
+              var room=$(room_elmt).find(".room").text();
+              $(room_elmt).find('span[id^=socket_id]').each(function(j,socket_elmt) {
+                  sockets[$(socket_elmt).text()] = {'roomId': room };
+                  // do more
+              });
+              var student_ids = $(room_elmt).find('span[id^=student_id]').map(function(idx, elem) {
+                return {'id': $(elem).text()};
+              }).get();
+          
+              rooms[room] = student_ids;
+            });
+        
+            // encode to JSON format
+            var rooms_json = JSON.stringify(rooms,null,'\t');
+            var sockets_json = JSON.stringify(sockets,null,'\t');
+            $('#printCode').html(sockets_json);
+          );
         });
 
         console.log(data);
@@ -73,23 +91,6 @@ angular.module('whiteboard-admin')
           });
         });
         $('.roomsList').on('mouseup', function() {
-          $('.roomList').each(function(i,room_elmt) { 
-            var room=$(room_elmt).find(".room").text();
-            $(room_elmt).find('span[id^=socket_id]').each(function(j,socket_elmt) {
-                sockets[$(socket_elmt).text()] = {'roomId': room };
-                // do more
-            });
-            var student_ids = $(room_elmt).find('span[id^=student_id]').map(function(idx, elem) {
-              return {'id': $(elem).text()};
-            }).get();
-        
-            rooms[room] = student_ids;
-          });
-      
-          // encode to JSON format
-          var rooms_json = JSON.stringify(rooms,null,'\t');
-          var sockets_json = JSON.stringify(sockets,null,'\t');
-          $('#printCode').html(sockets_json);
         });
         $('#generateJSON').click(function() {
       
