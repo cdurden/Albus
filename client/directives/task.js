41bfb9b185366ec904e018171722f7d11bc89d1c
Chris Durden <cdurden@gmail.com>
2020-03-23 16:29:24 -0500

    admin

--- client/directives/task.js
+++ client/directives/task.js
@@ -28,6 +28,7 @@ angular.module('whiteboard')
       Sockets.on('task', function (data) {
         console.log(data);
         TaskData.displayData(data);
+        scope.MathJax.typeset();
       })
 
     }
