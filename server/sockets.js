a769adef81306eee08e305f80c0e7fecaab381ef
Chris Durden <cdurden@gmail.com>
2020-03-22 20:06:42 -0500

    admin

--- server/sockets.js
+++ server/sockets.js
@@ -61,7 +61,7 @@ module.exports = function(server) {
       });
     }
     console.log("does the socket have an id?");
-    console.log(socket);
+    //console.log(socket);
     console.log(socket.id);
     rooms.placeSocket(socket);
  
