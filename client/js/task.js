6db1fb3fce590d4e3a4dc221bb31e83bb62a35b5
Chris Durden <cdurden@gmail.com>
2020-03-23 16:59:47 -0500

    admin

--- client/js/task.js
+++ client/js/task.js
@@ -4,3 +4,14 @@ function openTaskForm() {
 function closeTaskForm() {
       document.getElementById("task-popup").style.display = "none";
 }
+function toggleTaskForm() {
+  var x = document.getElementById("task-popup");
+  var btn = document.getElementById("task-toggle-button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    btn.innerHTML = "Hide task";
+  } else {
+    x.style.display = "none";
+    btn.innerHTML = "Show task";
+  }
+}
