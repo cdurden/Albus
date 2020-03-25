e7465943cac96a805bf1861997b4bcf31e97540c
Chris Durden <cdurden@gmail.com>
2020-03-24 23:35:55 -0500

    update-get-snow-qm-task-url

--- server/sockets.js
+++ server/sockets.js
@@ -215,9 +215,9 @@ module.exports = function(server) {
     socket.on('get-snow-qm-task', function(data){
       console.log("getting snow-qm task");
       request({
-        url: "https://dev.algebra742.org:444/api/snow-qm-task/",
+        url: "https://dev.algebra742.org:444/api/snow-qm-task/"+data['collection_id']+"/"+data['task_id'],
         headers : { "Authorization" : "Bearer " + auth.token },
-        qs: { 'collection_id': data['collection_id'], 'task_id': data['task_id'] },
+        //qs: { 'collection_id': data['collection_id'], 'task_id': data['task_id'] },
         //json: true
       },
       function(error, response, body) {
