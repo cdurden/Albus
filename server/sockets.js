6b000a6de8bb9d17ba5cd4384259be1e1590010d
Chris Durden <cdurden@gmail.com>
2020-04-04 21:15:45 -0500

    lti api

--- server/sockets.js
+++ server/sockets.js
@@ -37,7 +37,7 @@ module.exports = function(server) {
 
   io.on('connection', function (socket) {
    // if ('passport' in socket.handshake.session && 'user' in socket.handshake.session.passport) {
-      api.getUserFromSession(socket.handshake.session, function(error, data) {
+      api.getApiUserFromSession(socket.handshake.session, function(error, data) {
           console.log(data);
           //client.hmset(socket.id, Object.entries(body_json['data'][0]).flat());
       });
