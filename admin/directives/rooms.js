e67117e4178c33d70c60a2f1e42cc21f2dc88e11
Chris Durden <cdurden@gmail.com>
2020-04-08 13:22:01 -0500

    sortable

--- admin/directives/rooms.js
+++ admin/directives/rooms.js
@@ -16,25 +16,21 @@ angular.module('whiteboard-admin')
           }
           rooms[data[socket].roomId].push({'socket_id': socket, 'data': data[socket]});
         }
-//        $scope.$apply(function() {
-          $scope.rooms = rooms;
-//          $scope.$emit("update_rooms", rooms);
-//        });
-          /*
-      $scope.$on("update_rooms", function (evt, rooms) {
-        console.log(rooms);
-        $scope.rooms = rooms
-      });
-      */
-      function create_sortables() {
-        let rooms = {};
-        let sockets = {};
-        $(".roomList").each(function(i, elmt) {
-          console.log("creating sortable on element:");
-          console.log(elmt);
-          Sortable.create(elmt, {
-            group: 'rooms',
-            onChange: function() {
+        $scope.rooms = rooms;
+        function loadSortableJS(callback) {
+          var sortableScripts = ["https://raw.githack.com/SortableJS/Sortable/master/Sortable.js", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"]
+          Promise.all(sortableScripts.map(function(script) {
+            return(angularLoad.loadScript(script).then(function(result) {
+              return result;
+            }));
+          })).then(function() {
+            var sortables = [];
+            console.log("creating sortables");
+            $('#generateJSON').click(function() {
+          
+              let rooms = {};
+              let sockets = {};
+               
               $('.roomList').each(function(i,room_elmt) { 
                 var room=$(room_elmt).find(".room").text();
                 $(room_elmt).find('span[id^=socket_id]').each(function(j,socket_elmt) {
@@ -52,15 +48,46 @@ angular.module('whiteboard-admin')
               var rooms_json = JSON.stringify(rooms,null,'\t');
               var sockets_json = JSON.stringify(sockets,null,'\t');
               $('#printCode').html(sockets_json);
-            },
+              Sockets.emit('assign_sockets_to_rooms', sockets);
+            });
           });
+          callback();
+        }
+        function createSortables() {
+          let rooms = {};
+          let sockets = {};
+          $(".roomList").each(function(i, elmt) {
+            console.log("creating sortable on element:");
+            console.log(elmt);
+            Sortable.create(elmt, {
+              group: 'rooms',
+              onChange: function() {
+                $('.roomList').each(function(i,room_elmt) { 
+                  var room=$(room_elmt).find(".room").text();
+                  $(room_elmt).find('span[id^=socket_id]').each(function(j,socket_elmt) {
+                      sockets[$(socket_elmt).text()] = {'roomId': room };
+                      // do more
+                  });
+                  var student_ids = $(room_elmt).find('span[id^=student_id]').map(function(idx, elem) {
+                    return {'id': $(elem).text()};
+                  }).get();
+              
+                  rooms[room] = student_ids;
+                });
+            
+                // encode to JSON format
+                var rooms_json = JSON.stringify(rooms,null,'\t');
+                var sockets_json = JSON.stringify(sockets,null,'\t');
+                $('#printCode').html(sockets_json);
+              },
+            });
+          });
+        }
+        $scope.$watch("rooms", function (value) {//I change here
+          var val = value || null;            
+          if (val)
+            loadSortableJS(createSortables);
         });
-      }
-      $scope.$watch("rooms", function (value) {//I change here
-        var val = value || null;            
-        if (val)
-          create_sortables();
-      });
         console.log(data);
         console.log(rooms);
       });
@@ -93,39 +120,6 @@ angular.module('whiteboard-admin')
           else return callback();
       }
       */
-      var sortableScripts = ["https://raw.githack.com/SortableJS/Sortable/master/Sortable.js", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"]
-      Promise.all(sortableScripts.map(function(script) {
-        return(angularLoad.loadScript(script).then(function(result) {
-          return result;
-        }));
-      })).then(function() {
-        var sortables = [];
-        console.log("creating sortables");
-        $('#generateJSON').click(function() {
-      
-          let rooms = {};
-          let sockets = {};
-           
-          $('.roomList').each(function(i,room_elmt) { 
-            var room=$(room_elmt).find(".room").text();
-            $(room_elmt).find('span[id^=socket_id]').each(function(j,socket_elmt) {
-                sockets[$(socket_elmt).text()] = {'roomId': room };
-                // do more
-            });
-            var student_ids = $(room_elmt).find('span[id^=student_id]').map(function(idx, elem) {
-              return {'id': $(elem).text()};
-            }).get();
-        
-            rooms[room] = student_ids;
-          });
-      
-          // encode to JSON format
-          var rooms_json = JSON.stringify(rooms,null,'\t');
-          var sockets_json = JSON.stringify(sockets,null,'\t');
-          $('#printCode').html(sockets_json);
-          Sockets.emit('assign_sockets_to_rooms', sockets);
-        });
-      });
           
         /*
       injectLibsFromStack(function() {
