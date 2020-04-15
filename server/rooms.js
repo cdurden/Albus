faedaae11585b2e4d31acd4ec67572cb86b1a141
Chris Durden <cdurden@gmail.com>
2020-04-15 13:53:55 -0500

    assign tasks to sockets

--- server/rooms.js
+++ server/rooms.js
@@ -29,7 +29,7 @@ function assignRoomToSocket(socket, roomId, callback) {
       getRoomData(roomId, function (room) {
         room[socket.id] = {};
         socket.emit('showExisting', room);
-        callback();
+        callback && callback();
       });
     });
   }
