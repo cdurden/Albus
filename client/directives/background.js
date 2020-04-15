d32e79039813bdee815a4cb895f6a57a7e1a7894
Chris Durden <cdurden@gmail.com>
2020-04-15 09:32:12 -0500

    adding background directive

---
+++ client/directives/background.js
@@ -0,0 +1,31 @@
+angular.module('whiteboard')
+.directive('wbBackground', ['Sockets', function (Sockets) {
+  return {
+    restrict: 'A',
+    //require: ['wbFeed'],
+    //replace: true,
+    templateUrl: './templates/background.html',
+    controller: function (MessageHandler) {
+      this.handleEvent = function (ev) {
+        MessageHandler['feed'](ev);
+      }
+    },
+    scope: {},
+    link: function (scope, element, attrs, ctrls) {
+        /*
+      var feedCtrl = ctrls[0];
+      FeedData.createFeed(element);
+      FeedData.getForm().bind("submit",feedCtrl.handleEvent);
+      */
+      scope.$watchGroup(["$parent.tasks", "$parent.i"], function(newValues) {
+        var task = newValues[0][newValues[1]];
+        scope.task = task;
+      });
+        /*
+      Sockets.on('feed message', function (msg) {
+        FeedData.displayMessage(msg);
+      })
+      */
+    }
+  }
+}]);
