230f0761eef1badc3f18df3aeaaf5c1bd31d2a04
Chris Durden <cdurden@gmail.com>
2020-04-15 14:10:29 -0500

    assign tasks to sockets

--- server/rooms.js
+++ server/rooms.js
@@ -171,6 +171,8 @@ var roomsManager = {
 
   deleteShape: function (shape, socket) {
     console.log("deleting shape "+shape.myid);
+    console.log("roomId: "+socket.room);
+    console.log("socketId: "+shape.socketId);
     console.log(rooms[socket.room][shape.socketId]);
     delete rooms[socket.room][shape.socketId][shape.myid];
     client.set(socket.room, JSON.stringify(rooms[socket.room])); 
