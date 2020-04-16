6523f78127583e605a72e4643aa81d1f937c5ac8
Chris Durden <cdurden@gmail.com>
2020-04-15 19:25:54 -0500

    adjusting viewbox based on background-container

--- client/directives/background.js
+++ client/directives/background.js
@@ -49,7 +49,7 @@ angular.module('whiteboard')
         scope.task = task;
       });
       bg_ctnr_elmt = document.getElementById('background-container')
-      ResizeSensor.create(bg_ctnr_elmt, handleBackgroundResize);
+      new ResizeSensor(bg_ctnr_elmt, handleBackgroundResize);
       /*
       Sockets.on('feed message', function (msg) {
         FeedData.displayMessage(msg);
