9d70cc86b41a61899094d935563406c61a76f33f
Chris Durden <cdurden@gmail.com>
2020-04-19 17:15:47 -0500

    get users

--- admin/directives/assignments.js
+++ admin/directives/assignments.js
@@ -8,17 +8,21 @@ angular.module('whiteboard-admin')
     controller: function ($scope) {
       $scope.assignments = {};
       $scope.users = [];
+      Sockets.on('assignments', function (data) {
+        console.log(data);
+        for (assignment in data) {
+            $scope.assignments[assignment] = $scope.assignments[assignment] || {}
+        }
+      }
       Sockets.on('users', function (data) {
         console.log(data);
-        assignments = {};
         $scope.users = data;
         for (user in data) {
-          if (!(user.assignment in assignments)) {
-              assignments[user.assignment] = [];
+          if (!(user.assignment in $scope.assignments)) {
+              $scope.assignments[user.assignment] = [];
           }
-          assignments[user.assignment].push(user);
+          $scope.assignments[user.assignment].push(user);
         }
-        $scope.assignments = assignments;
         function loadSortableJS(callback) {
           var sortableScripts = ["https://raw.githack.com/SortableJS/Sortable/master/Sortable.js", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"]
           Promise.all(sortableScripts.map(function(script) {
@@ -71,65 +75,10 @@ angular.module('whiteboard-admin')
         console.log(assignments);
       });
       Sockets.emit('getUsers');
+      Sockets.emit('getAssignments');
     },
     link: function(scope, element, attrs) {
       console.log("calling link function");
-        /*
-      function injectLibsFromStack(callback){
-          if(libs.length > 0){
-      
-            //grab the next item on the stack
-            var nextLib = libs.shift();
-            var headTag = document.getElementsByTagName('head')[0];
-      
-            //create a script tag with this library
-            var scriptTag = document.createElement('script');
-            scriptTag.src = nextLib;
-      
-            //when successful, inject the next script
-            scriptTag.onload = function(e){
-              console.log("---> loaded: " + e.target.src);
-              injectLibsFromStack(callback);
-            };
-      
-            //append the script tag to the <head></head>
-            headTag.appendChild(scriptTag);
-            console.log("injecting: " + nextLib);
-          }
-          else return callback();
-      }
-      */
-          
-        /*
-      injectLibsFromStack(function() {
-        var sortables = [];
-        console.log("creating sortables");
-        $('#generateJSON').click(function() {
-      
-          let assignments = {};
-          let users = {};
-           
-          $('.assignmentList').each(function(i,assignment_elmt) { 
-            var assignment=$(assignment_elmt).find(".assignment").text();
-            $(assignment_elmt).find('span[id^=user_id]').each(function(j,user_elmt) {
-                users[$(user_elmt).text()] = {'assignmentId': assignment };
-                // do more
-            });
-            var student_ids = $(assignment_elmt).find('span[id^=student_id]').map(function(idx, elem) {
-              return {'id': $(elem).text()};
-            }).get();
-        
-            assignments[assignment] = student_ids;
-          });
-      
-          // encode to JSON format
-          var assignments_json = JSON.stringify(assignments,null,'\t');
-          var users_json = JSON.stringify(users,null,'\t');
-          $('#printCode').html(users_json);
-          Sockets.emit('assign_users_to_assignments', users);
-        });
-      });
-      */
     },
   }
 }]);
