d0a48cf14ac86f39dbfa6307a17787d4b4442696
Chris Durden <cdurden@gmail.com>
2020-04-28 11:13:17 -0500

    adding navigation

--- client/directives/tasks.js
+++ client/directives/tasks.js
@@ -64,8 +64,8 @@ angular.module('whiteboard')
 		//}
 
 		pointerEvents.forEach( function( eventName ) {
-			controlsPrev.forEach( function( el ) { el.addEventListener( eventName, onNavigatePrevClicked, false ); } );
-			controlsNext.forEach( function( el ) { el.addEventListener( eventName, onNavigateNextClicked, false ); } );
+			controlsPrev.each( function( el ) { el.addEventListener( eventName, onNavigatePrevClicked, false ); } );
+			controlsNext.each( function( el ) { el.addEventListener( eventName, onNavigateNextClicked, false ); } );
 		} );
 
 	}
