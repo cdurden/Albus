027a22a1e010eb8f2d6404d42e7a03c6cfef0c31
Chris Durden <cdurden@gmail.com>
2020-04-28 04:25:17 -0500

    saving board to api

--- server/sockets.js
+++ server/sockets.js
@@ -47,10 +47,10 @@ module.exports = function(server) {
       });
     });
   }
-  function saveBoardToApi(socket, boardId, taskId) {
+  function saveBoardToApi(socket, data) {
     return new Promise(resolve => {
-      board = rooms.getBoardStorage(rooms.getRoomId(socket), boardId);
-      api.saveBoard(socket.handshake.session, board, data, function(err, data) {
+      shapeStorage = rooms.getBoardStorage(rooms.getRoomId(socket), boardId);
+      api.saveBoard(socket.handshake.session, shapeStorage, data, function(err, data) {
           console.log("Board saved");
           console.log(data);
           resolve(data);
@@ -366,7 +366,7 @@ module.exports = function(server) {
     })
     socket.on('submit', function(data){
       //console.log(data);
-      saveBoardToApi(socket, data.boardId, data.taskId).then(function(board) {
+      saveBoardToApi(socket, data).then(function(board) {
           console.log(board);
           data.board_id = board.id;
           api.submit(socket.handshake.session, data, function(error, data) {
