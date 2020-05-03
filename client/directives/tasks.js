8da84f2f333af49f76cb4796b008461a1df54db8
Chris Durden <cdurden@gmail.com>
2020-05-02 20:33:41 -0500

    debugging actAsUser

--- client/directives/tasks.js
+++ client/directives/tasks.js
@@ -18,14 +18,6 @@ angular.module('whiteboard')
       $scope.i = 0;
       $scope.uploader = new FileUploader();
       //angular.element($document[0].body).attr("nv-file-drop",true).attr("uploader","uploader").attr("options", "{url: '/upload', 'autoUpload': true}");
-$window.addEventListener("dragover",function(e){
-  e = e || event;
-  e.preventDefault();
-},false);
-$window.addEventListener("drop",function(e){
-  e = e || event;
-  e.preventDefault();
-},false);
       //BoardData.loadBoard(Object.keys($scope.boards)[0]);
       //$scope.taskBoards = BoardData.getTaskBoards();
       //$scope.boards = [];
