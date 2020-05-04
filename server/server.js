402c233c1595e5eebf37d62977855a2d597d38bf
Chris Durden <cdurden@gmail.com>
2020-05-03 22:45:13 -0500

    assigning users to rooms and then using user assignment to assign sockets

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ passport.use('lti-strategy', new CustomStrategy(
 ));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/upload', api.uploadProxy);
+router.post('/upload', api.uploadProxy);
 
 app.use(express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/../client'));
