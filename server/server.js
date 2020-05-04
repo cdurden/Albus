ac5933a0708b8415dfae1b25e71655c6405ca0f9
Chris Durden <cdurden@gmail.com>
2020-05-04 00:24:22 -0500

    assigning users to rooms and then using user assignment to assign sockets

--- server/server.js
+++ server/server.js
@@ -151,8 +151,8 @@ app.use(fileUpload({
         tempFileDir : '/tmp/'
 }));
 //app.use('/upload', upload.uploadHandler);
-//app.use('/upload', api.uploadHandler);
-app.use('/upload', api.uploadProxy);
+app.use('/upload', api.uploadHandler);
+//app.use('/upload', api.uploadProxy);
 app.get('/', function (req, res) {
   console.log("responding to GET request at /");
   console.log(req.user);
