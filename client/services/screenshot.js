77efb10733fa709fa8cf5a244a2b218c04fcd7c2
Chris Durden <cdurden@gmail.com>
2020-05-05 13:22:22 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -12,21 +12,24 @@ $window.onload = () => {
 function screenshot() {
   var paper = BoardData.getBoard();
   var backgroundImg = $document.find(".background-image img");
-  paper.image(backgroundImg.attr('src'), 0, 0, backgroundImg[0].width, backgroundImg[0].height).toBack();
+  //paper.image(backgroundImg.attr('src'), 0, 0, backgroundImg[0].width, backgroundImg[0].height).toBack();
   var svg = BoardData.getCanvas()[0];
+  paper.image(backgroundImg.attr('src'), 0, 0, svg.clientWidth, svg.clientHeight).toBack();
   //canvas.width = svg.width
   //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
   //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
   var data = (new XMLSerializer()).serializeToString(svg); 
   v = canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
   v.start();
-  
   var theImage=document.getElementById("screenshot");
-      theImage.src=$canvas[0].toDataURL();
   theImage.onload = function() {
           var w = $window.open("");
           w.document.write(this.outerHTML);
   };
+  setTimeout(function() {
+      theImage.src=$canvas[0].toDataURL();
+  }, 1000);
+  
 }
   return {
       screenshot: screenshot,
