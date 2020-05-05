d4791e2bb6c3b4a1990e385799128625df562312
Chris Durden <cdurden@gmail.com>
2020-05-05 12:41:15 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -1,12 +1,12 @@
 angular.module('whiteboard.services.screenshot', [])
 .factory('Screenshot', ['BoardData', '$window', '$document', function (BoardData, $window, $document) {
 var canvg;
-var canvas; // Create a Canvas element.
+var $canvas; // Create a Canvas element.
 var ctx; // For Canvas returns 2D graphic.
 $window.onload = () => {
-  canvas = angular.element('<canvas>'); // Create a Canvas element.
-  $document.body.append(canvas);
-  ctx = canvas.getContext('2d'); // For Canvas returns 2D graphic.
+  $canvas = angular.element('<canvas>'); // Create a Canvas element.
+  $document.body.append(canvas)[0];
+  ctx = canvas[0].getContext('2d'); // For Canvas returns 2D graphic.
   canvg = $window.canvg; // Render SVG on Canvas.
 };
 function screenshot() {
@@ -15,7 +15,7 @@ function screenshot() {
   //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
   //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
   var data = (new XMLSerializer()).serializeToString(svg); 
-  canvg.Canvg.fromString(canvas, data); // Render SVG on Canvas.
+  canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
   var theImage=document.getElementById("screenshot");
       theImage.src=canvas.toDataURL();
   theImage.onload = function() {
