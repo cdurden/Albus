ba57e32916db906385925f1e67549033c0a4a97e
Chris Durden <cdurden@gmail.com>
2020-05-05 11:18:10 -0500

    handling getBoard case where DB returns no result

--- server/server.js
+++ server/server.js
@@ -256,7 +256,7 @@ app.get('/:id/screenShot', function (req, res) {
     const tmp = require('tmp');
     const tmpobj = tmp.fileSync();
     console.log('File: ', tmpobj.name);
-    webshot('localhost:3000/' + req.params.id, tmpobj.name, function(err) {
+    webshot('localhost/' + req.params.id, tmpobj.name, function(err) {
     res.sendFile(tmpobj.name);
     //res.sendFile(tmpobj.name, { root: __dirname });
   });
