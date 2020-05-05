8d77b4e1f79e5e1562af44306e51d5ddccb8aced
Chris Durden <cdurden@gmail.com>
2020-05-05 12:36:50 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -1,15 +1,17 @@
 angular.module('whiteboard.services.screenshot', [])
-.factory('Screenshot', ['BoardData', '$window', function (BoardData, $window) {
+.factory('Screenshot', ['BoardData', '$window', '$document', function (BoardData, $window, $document) {
 var canvg;
 var canvas; // Create a Canvas element.
 var ctx; // For Canvas returns 2D graphic.
 $window.onload = () => {
-  canvas = document.createElement('canvas'); // Create a Canvas element.
+  canvas = $document.createElement('canvas'); // Create a Canvas element.
+  $document.body.append(canvas);
   ctx = canvas.getContext('2d'); // For Canvas returns 2D graphic.
   canvg = $window.canvg; // Render SVG on Canvas.
 };
 function screenshot() {
   var svg = BoardData.getCanvas()[0];
+  canvas.width = svg.width
   //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
   //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
   var data = (new XMLSerializer()).serializeToString(svg); 
