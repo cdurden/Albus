9cd55548de16128b1f6fc17cfbdd2853e6a8ab4d
Chris Durden <cdurden@gmail.com>
2020-05-05 16:03:52 -0500

    adding

--- server/server.js
+++ server/server.js
@@ -119,6 +119,7 @@ io.of('/admin').use(sharedsession(session, { // FIXME: feeding off of the sessio
 }));
 io.on('connection', (socket) => {
     console.log("Got connection request");
+    console.log(socket.handshake.session);
     socket.use((packet, next) => {
         if ('passport' in socket.handshake.session && 'user' in socket.handshake.session.passport) { 
             next();
