e35ed09b4a11d5889c42899d768dacecc0cd21c9
Chris Durden <cdurden@gmail.com>
2020-05-05 12:57:56 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -15,10 +15,13 @@ function screenshot() {
   //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
   //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
   var data = (new XMLSerializer()).serializeToString(svg); 
-  canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
+  v = canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
+  v.start();
+  
   var theImage=document.getElementById("screenshot");
       theImage.src=$canvas[0].toDataURL();
   theImage.onload = function() {
+    context.drawImage(theImage, 0, 0);
           var w = $window.open("");
           w.document.write(this.outerHTML);
   };
