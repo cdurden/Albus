1ef41cb5e6ab550598c6d06aa12f4005859697c8
Chris Durden <cdurden@gmail.com>
2020-05-05 12:26:37 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -8,22 +8,18 @@ $window.onload = () => {
   ctx = canvas.getContext('2d'); // For Canvas returns 2D graphic.
   canvg = $window.canvg; // Render SVG on Canvas.
 };
-function SVG2PNG(svg, callback) {
-  var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
-  var data = bp+svg.outerHTML; // Get SVG element as HTML code.
-  canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
-  callback(canvas); // Execute callback function.
-}
 function screenshot() {
   var svg = BoardData.getCanvas()[0];
-  SVG2PNG(svg, function() {
-      var theImage=document.getElementById("screenshot");
-          theImage.src=canvas.toDataURL();
-      theImage.onload = function() {
-              var w = $window.open("");
-              w.document.write(this.outerHTML);
-      };
-  })
+  //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
+  //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
+  var data = (new XMLSerializer()).serializeToString(svg); 
+  canvg.Canvg.fromString(ctx, data); // Render SVG on Canvas.
+  var theImage=document.getElementById("screenshot");
+      theImage.src=canvas.toDataURL();
+  theImage.onload = function() {
+          var w = $window.open("");
+          w.document.write(this.outerHTML);
+  };
 }
   return {
       screenshot: screenshot,
