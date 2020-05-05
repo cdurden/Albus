31ad4fc01ea143e8e1449fce4665272d29760304
Chris Durden <cdurden@gmail.com>
2020-05-05 11:52:00 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -1,10 +1,10 @@
 angular.module('whiteboard.services.screenshot', [])
-.factory('Screenshot', ['BoardData', function (BoardData) {
+.factory('Screenshot', ['BoardData', '$window', function (BoardData, $window) {
 function SVG2PNG(svg, callback) {
   var canvas = document.createElement('canvas'); // Create a Canvas element.
   var ctx = canvas.getContext('2d'); // For Canvas returns 2D graphic.
   var data = svg.outerHTML; // Get SVG element as HTML code.
-  canvg(canvas, data); // Render SVG on Canvas.
+  $window.canvg(canvas, data); // Render SVG on Canvas.
   callback(canvas); // Execute callback function.
 }
 function screenshot() {
