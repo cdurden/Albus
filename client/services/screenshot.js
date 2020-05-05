8ddbb6d847134ab6dfa6b6280d34ecd8bdd838bd
Chris Durden <cdurden@gmail.com>
2020-05-05 14:12:18 -0500

    handling screenshots

--- client/services/screenshot.js
+++ client/services/screenshot.js
@@ -14,15 +14,17 @@ function screenshot() {
   var canvas = BoardData.getCanvas()[0];
   var box = canvas.getAttribute('viewBox');
   let [x, y, w, h] = box.split(/\s+|,/);
-  var paper = Raphael($document.find("#screenshot-container")[0]);
-  paper.canvas = canvas.cloneNode(true);
-  //paper.setViewBox(x, y, w, h, true);
   var backgroundImg = $document.find(".background-image img");
   //paper.image(backgroundImg.attr('src'), 0, 0, backgroundImg[0].width, backgroundImg[0].height).toBack();
   var svg = BoardData.getCanvas()[0];
   //var box = svg.getAttribute('viewBox');
   //let [x, y, w, h] = box.split(/\s+|,/);
-  paper.image(backgroundImg.attr('src'), 0,0,backgroundImg[0].naturalWidth,backgroundImg[0].naturalHeight).toBack();
+  var paper = Raphael($document.find("#screenshot-container")[0]);
+  paper.canvas = canvas.cloneNode(true);
+  w = backgroundImg[0].naturalWidth;
+  h = backgroundImg[0].naturalHeight;
+  paper.setViewBox(0, 0, w, h, true);
+  paper.image(backgroundImg.attr('src'), 0,0,w,h).toBack();
   //canvas.width = svg.width
   //var bp = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">;';
   //var data = bp+svg.outerHTML; // Get SVG element as HTML code.
