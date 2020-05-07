d7e48535bab8b3ac8aaf346eb649bf04bf8cd62a
Chris Durden <cdurden@gmail.com>
2020-05-07 12:05:00 -0500

    -m

--- server/util.js
+++ server/util.js
@@ -3,7 +3,7 @@ const api = require('./api');
 api.getBoards(function(err, boards) {
     session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
     console.log(boards);
-    for (board in Object.values(boards)) {
+    for (board of boards) {
         console.log(Object.keys(board));
         data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
         api.submit(session, data, function(err, res) {
