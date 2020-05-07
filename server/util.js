17e7b5e51128cd98bb70e88fe903be98606ab6cc
Chris Durden <cdurden@gmail.com>
2020-05-07 15:32:03 -0500

    -m

--- server/util.js
+++ server/util.js
@@ -5,6 +5,7 @@ api.getBoards(function(err, boards) {
         session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
         //console.log(boards);
         for (board of boards) {
+            console.log(board.task_id);
             if (board.task_id) {
                 console.log("Submitting work on board "+board.boardId);
                 data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
