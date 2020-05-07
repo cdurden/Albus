7489d4a1b9c2472e29646f5fccf726f2bd93975a
Chris Durden <cdurden@gmail.com>
2020-05-07 15:34:15 -0500

    -m

--- server/util.js
+++ server/util.js
@@ -1,19 +1,22 @@
 const api = require('./api');
 
-api.getBoards(function(err, boards) {
+function submitBoards() {
     return new Promise(resolve => {
-        session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
-        //console.log(boards);
-        for (board of boards) {
-            console.log(board.task_id);
-            if (board.task_id) {
-                console.log("Submitting work on board "+board.boardId);
-                data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
-                api.submit(session, data, function(err, res) {
-                    console.log(res)
-                    resolve(res)
-                })
+        api.getBoards(function(err, boards) {
+            session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
+            //console.log(boards);
+            for (board of boards) {
+                console.log(board.task_id);
+                if (board.task_id) {
+                    console.log("Submitting work on board "+board.boardId);
+                    data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
+                    api.submit(session, data, function(err, res) {
+                        console.log(res)
+                        resolve(res)
+                    })
+                }
             }
-        }
-    })
-}).then(process.exit);
+        });
+    });
+}
+submitBoards().then(process.exit);
