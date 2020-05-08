0c257088e3300dec87d59ecf2f764bef8b64d9a7
Chris Durden <cdurden@gmail.com>
2020-05-07 23:29:57 -0500

    adding feedback pane

--- server/util.js
+++ server/util.js
@@ -1,25 +1,25 @@
 const api = require('./api');
+var session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
+async function submitBoard(board) {
+    if (board.task_id) {
+        console.log("Submitting work on board "+board.boardId+" for user "+board.user.lti_user_id);
+        data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
+        session.actingAsUser = board.user.lti_user_id;
+        //console.log(session.actingAsUser);
+        api.submit(session, data, function(err, res) {
+            console.log(res)
+            resolveBoard(res);
+        })
+    }
+}
 
-function submitBoards() {
+async function submitBoards() {
     return new Promise(resolve => {
         api.getBoards(function(err, boards) {
-            session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
             //console.log(boards);
-            Promise.all(boards.map(board => {
-                return new Promise(resolveBoard => {
-                    //console.log(board.task_id);
-                    if (board.task_id) {
-                        console.log("Submitting work on board "+board.boardId+" for user "+board.user.lti_user_id);
-                        data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
-                        session.actingAsUser = board.user.lti_user_id;
-                        //console.log(session.actingAsUser);
-                        api.submit(session, data, function(err, res) {
-                            console.log(res)
-                            resolveBoard(res);
-                        })
-                    }
-                });
-            })).then(resolve(boards));
+            for (board of boards) {
+                await submitBoard(board);
+            }
         });
     });
 }
