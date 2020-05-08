208fcc4adf78055e06d60b392a2ef08eca8108fa
Chris Durden <cdurden@gmail.com>
2020-05-08 12:33:51 -0500

    revising getBoardStorage to fallback to redis, making it async

--- server/sockets.js
+++ server/sockets.js
@@ -699,7 +699,7 @@ function get_all_data_by_socket(socket, callback) {
     });
     socket.on('loadBoardFromApi', function(boardId) {
       api.getBoard(socket.handshake.session, boardId, function(err, board) {
-        if (typeof board.boardId === 'undefined') {
+        if (typeof (board || {}).boardId === 'undefined') {
           /*
             board = {
                 'boardId': boardId,
