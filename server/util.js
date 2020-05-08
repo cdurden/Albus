8d1bcf813fdf3badafdb7c1778a7ed006cd3869b
Chris Durden <cdurden@gmail.com>
2020-05-07 23:47:45 -0500

    adding feedback pane

--- server/util.js
+++ server/util.js
@@ -1,24 +1,24 @@
 const api = require('./api');
-var session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
 async function submitBoard(board) {
+    var session = { passport: { user: "86258941::65ea761411d6325962ddba010329193a" } };
     if (board.task_id) {
         data = { taskId: board.task_id, lti_user_id: board.user.lti_user_id, board_id: board.id }
         session.actingAsUser = board.user.lti_user_id;
         console.log("Submitting work on board "+board.boardId+" for user "+board.user.lti_user_id);
         //console.log(session.actingAsUser);
         api.submit(session, data, function(err, res) {
-            console.log(res)
-            resolveBoard(res);
+            console.log(res);
+            return(res);
         })
     }
 }
 async function getBoards() {
-    return new Promise(resolve => {
+    //return new Promise(resolve => {
     api.getBoards((err, boards) => {
         //console.log(boards);
         resolve(boards)
     });
-    })
+    //})
 }
 
 async function submitBoards() {
