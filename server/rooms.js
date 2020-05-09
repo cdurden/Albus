316f97678e14e7dcf1980e91e0af89a497ff4425
Chris Durden <cdurden@gmail.com>
2020-05-08 19:05:21 -0500

    linking board to task by source to enable API independence

--- server/rooms.js
+++ server/rooms.js
@@ -229,7 +229,7 @@ function loadBoardFromRedis(roomId, boardId, callback) {
       }
       if (reply) {
         storedBoard = JSON.parse(reply);
-        rooms.[roomId][boardId].shapeStorage = storedBoard;
+        rooms[roomId][boardId].shapeStorage = storedBoard;
         //_.extend(rooms[roomId][boardId].shapeStorage, storedBoard.shapeStorage);
           //callback && callback(rooms[roomId][boardId]);
           callback && callback(true);
