e68ae6fc23c6e8041efcbcd2af19ef80568350ff
Chris Durden <cdurden@gmail.com>
2020-05-10 22:40:20 -0500

    storing tags to feedback data

--- server/sockets.js
+++ server/sockets.js
@@ -788,9 +788,11 @@ module.exports = function(server, session) {
             if (feedback) {
                 board = feedback.board
                 rooms.loadBoard(socket.room, board, function() {
-                  assets.getTaskAssets([board.task.source]).then(function(taskAssets) {
-                      socket.emit('tasks', taskAssets);
-                  });
+                  if (typeof (board.task || {}).source  !== 'undefined') {
+                      assets.getTaskAssets([board.task.source]).then(function(taskAssets) {
+                          socket.emit('tasks', taskAssets);
+                      });
+                  }
                   console.log("Sending board to client");
                   console.log(board);
                   socket.emit('boards', [board]);
