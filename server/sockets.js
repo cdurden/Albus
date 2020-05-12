1602aebf2c25f25f5017b101cdcead8dc8cefee4
Chris Durden <cdurden@gmail.com>
2020-05-12 14:55:46 -0500

    board has no task, so substituting feedback.submission.board.task

--- server/sockets.js
+++ server/sockets.js
@@ -796,7 +796,7 @@ module.exports = function(server, session) {
                   //if (typeof (board.task || {}).source  !== 'undefined') {
                       //assets.getTaskAssets([board.task.source]).then(function(taskAssets) {
                       console.log(board);
-                      assets.getTaskAssets([feedback.board.task.source]).then(function(taskAssets) {
+                      assets.getTaskAssets([feedback.submission.board.task.source]).then(function(taskAssets) {
                           socket.emit('tasks', taskAssets);
                       });
                   //}
