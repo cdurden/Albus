d3b877516fd6cd81a75aaf44fbb3f79e2394ac20
Chris Durden <cdurden@gmail.com>
2020-05-23 00:35:15 -0500

    separating assignment boards from free boards

--- client/directives/feedback.js
+++ client/directives/feedback.js
@@ -121,7 +121,7 @@ angular.module('whiteboard')
             $scope.submissionBoard = board;
             $scope.submission = board.submission;
             var nFeedback = (board.feedbackList || []).length
-            if (nFeedback > 0) {
+            if (nFeedback > 0 && $scope.showFeedbackBoard) {
                 $scope.boardData.boardId = board.feedbackList[nFeedback-1].board.boardId;
             }
         }
