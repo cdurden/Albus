6245fc247bb78d8336641cf174df9369ab75a5e4
Chris Durden <cdurden@gmail.com>
2020-05-22 22:52:30 -0500

    separating assignment boards from free boards

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,14 @@
 var redis = require("redis");
 var client = redis.createClient();
 
+client.hmsetOrig = client.hmset;
+client.hmset = function() {
+    if(arguments.includes(undefined)) {
+        throw Error("undefined argument passed to hmset. Arguments: "+arguments.join());
+    }
+    client.hmsetOrig.apply(null, arguments);
+}
+
 client.on("error", function (err) {
   console.log("Error " + err);
 });
